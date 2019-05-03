b90264aa039e843131dbba4784751fef637d97b5
Matt8198 <matthias.ambroise@gmail.com>
2019-05-03 20:32:09 +0200

    Probleme avec le cast du NullV à fix

--- Gen.java
+++
@@ -1,5 +0,0 @@
-import java.util.*; 
-public class Gen { 
-public static LinkedList<Instr> code = 
-LLE.add_elem(new Push(),LLE.add_elem(new Push(),LLE.add_elem(new Cur(LLE.add_elem(new Cur(LLE.add_elem(new Fst(),LLE.add_elem(new Snd(),LLE.add_elem(new Return(),LLE.empty())))),LLE.add_elem(new Return(),LLE.empty()))),LLE.add_elem(new Swap(),LLE.add_elem(new Quote(new IntV(2)),LLE.add_elem(new Cons(),LLE.add_elem(new App(),LLE.add_elem(new Swap(),LLE.add_elem(new Quote(new IntV(3)),LLE.add_elem(new Cons(),LLE.add_elem(new App(),LLE.empty()))))))))))); 
-}
